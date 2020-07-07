8720e0fe316e7f2816249c34aaa131b1da8014ba
michael emmons <memmons18@gmail.com>
2020-07-06 23:42:03 -0400

    added dynamic input to the state page

--- app/public/js/state.js
+++ app/public/js/state.js
@@ -1,10 +1,24 @@
 let path = window.location.pathname;
 let splitpath = path.split("/");
+//get url path to obtain state name
 let covidState = splitpath[splitpath.length - 1];
 console.log(splitpath[splitpath.length - 1]);
 
+//make call to server using state needing info on
 $.ajax({ url: "/api/state/" + covidState, method: "get" }).then(function (
   stateData
 ) {
-  console.log(stateData);
+  populateData(stateData);
 });
+
+//dynamically populate info to the site
+function populateData(stateData) {
+  for (let key in stateData) {
+    let title = $("<h2>");
+    let li = $("<li>");
+    title.text(key + ": " + stateData[key]);
+
+    li.append(title);
+    $(".state-general").append(li);
+  }
+}
--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -2,5 +2,6 @@ module.exports = function (app) {
   app.get("/api/state/:state", function (req, res) {
     console.log(req.params.state);
     //get state and begin running it through api calls to gather information about covid, general state info (population, governer, any updated news (state of emergencies)), a weeks worth of data, and nearby covid testing centers
+    res.json({ state: req.params.state, population: 300000 });
   });
 };
