af9769fc299d2ef9e6c345a66ea8734ca40fcc6f
michael emmons <memmons18@gmail.com>
2020-07-10 17:10:11 -0400

    cleaned up state.js to be to be more efficient with element create function

--- app/public/js/state.js
+++ app/public/js/state.js
@@ -11,37 +11,43 @@ let stateNoSpace;
 // });
 
 $.get("/api/stateData", function (data) {
-  populateStateData(data);
+  populateStateCovidData(data);
+  populateStateInfo(data);
   stateCovidSite(data);
 });
 
+function populateStateInfo(data) {
+  let title = createElement("<h2>", data.state);
+  let population = createElement("<h2>", "population: ");
+  let governor = createElement("<h2>", "Governor: ");
+
+  $(".state-general").append(title, population, governor);
+}
+
 //dynamically populate info to the site
-function populateStateData(stateData) {
+function populateStateCovidData(stateData) {
   stateNoSpace = stateData.state.split(" ").join("");
   let { covidData } = stateData;
   getFlagInfo(stateData.state);
 
-  let li = $("<li>");
-  let title = $("<h2>");
-  let active = $("<h2>");
-  let confirmed = $("<h2>");
-  let deaths = $("<h2>");
-  let death_diff = $("<h2>");
-  let confirm_diff = $("<h2>");
-
-  title.text(stateData.state);
-  active.text(`Active cases: ${covidData.active}`);
-  confirmed.text(`Confirmed cases: ${covidData.confirmed}`);
-  deaths.text(`Deaths: ${covidData.deaths}`);
-  death_diff.text(
+  let li = createElement("<li>");
+  let active = createElement("<h2>", `Active cases: ${covidData.active}`);
+  let confirmed = createElement(
+    "<h2>",
+    `Confirmed cases: ${covidData.confirmed}`
+  );
+  let deaths = createElement("<h2>", `Deaths: ${covidData.deaths}`);
+  let death_diff = createElement(
+    "<h2>",
     `Num of Deaths compared to previous day: ${covidData.death_diff}`
   );
-  confirm_diff.text(
+  let confirm_diff = createElement(
+    "<h2>",
     `Num of Confirmed cases compared to previous day: ${covidData.confirmed_diff}`
   );
 
-  li.append(title, active, confirmed, deaths, death_diff, confirm_diff);
-  $(".state-general").append(li);
+  li.append(active, confirmed, deaths, death_diff, confirm_diff);
+  $(".state-covid").append(li);
 }
 
 function getFlagInfo(state) {
@@ -64,15 +70,27 @@ function stateCovidSite(data) {
       }
     });
     populateToHTML(stateCovidUrl);
-    console.log(stateCovidUrl);
   });
 
   function populateToHTML(stateCovidlinks) {
     let link = stateCovidlinks[0].covid19Site;
-    let stateLink = $("<a>");
-    stateLink.text("View Official State Site");
-    stateLink.attr("href", link);
-    stateLink.attr("target", "_blank");
+    let stateLink = createElement("<a>", "View Official State Site", [
+      { attr: "href", value: link },
+      { attr: "target", value: "blank" },
+    ]);
     $(".state-covid").append(stateLink);
   }
+}
+
+function createElement(el, text, attr) {
+  let newEl = $(`${el}`);
+  if (text) {
+    newEl.text(text);
+  }
+  if (attr) {
+    attr.forEach((item) => {
+      newEl.attr(item.attr, item.value);
+    });
+  }
+  return newEl;
 }
